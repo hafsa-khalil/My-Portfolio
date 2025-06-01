64713c599b943e6a0ac4a299f36bd2f15476bed9
hafsa-khalil <151922788+hafsa-khalil@users.noreply.github.com>
2025-06-01 22:23:14 +0500

    Add files via upload

---
+++ assignment.js
@@ -0,0 +1,13 @@
+// code for buttons functionality
+
+document.getElementById("btn-projects").addEventListener("click", function () {
+  window.location.href = "projects.html";
+});
+
+document.getElementById("btn-cv").addEventListener("click", function () {
+  window.location.href = "cv.html";
+});
+
+document.getElementById("btn-contact").addEventListener("click", function () {
+  window.location.href = "contact.html";
+});
